967818ef919a0d7eb7b277aa8c54996fbeb1f39c
laryssavargas <laryssa.borth.vargas@escola.pr.gov.br>
2026-06-01 08:42:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meus objetivos do ano</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="conteudo-principal">
+        <h2 class="titulo-principal">Meus Objetivos do ano<span>_</span></h2>
+        <div class="botoes">
+            <button class="botao ativo">Cursos na Alura</button>
+            <button class="botao">Criar projetos em Javascript</button>
+            <button class="botao">Criar um portfolio</button>
+            <button class="botao">Atualizar meu currículo</button>
+        </div>
+    </section>
+</body>
+</html>.ativo{
+background-color: var(--botao-ativo);
+border-bottom: 4px solid var(--verde);
+}main.js    </section>
+        <script src="main.js"></script>
+</body>
+</html>const botoes = document.querySelectorAll(".botao");const botoes = document.querySelectorAll(".botao");
+console.log(botoes);const botoes = document.querySelectorAll(".botao");
+
+for()const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+}
